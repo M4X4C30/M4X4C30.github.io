456e6909ed98d437dd2e6146c6a28019071326ae
HeiseiAshford <134974285+M4X4C30@users.noreply.github.com>
2023-06-03 12:47:41 +0800

    added animations

--- responsive des/animations/animation.js
+++ responsive des/animations/animation.js
@@ -5,16 +5,20 @@ const observer = new IntersectionObserver((entries) => {
             if (entry.target.classList.contains('To_Scalein')) {
                 entry.target.classList.add('An_Scalein');
             }
-            if (entry.target.classList.contains('To_slidefromLeft')) {
-                entry.target.classList.add('An_slidefromLeft');
+            if (entry.target.classList.contains('To_slidetoRight')) {
+                entry.target.classList.add('An_slidetoRight');
             }
 
-            if (entry.target.classList.contains('To_slidefromRight')) {
-                entry.target.classList.add('An_slidefromRight');
+            if (entry.target.classList.contains('To_slidetoLeft')) {
+                entry.target.classList.add('An_slidetoLeft');
             }
 
             if (entry.target.classList.contains('To_Fadein')) {
                 entry.target.classList.add('An_Fadein');
+            }
+
+            if (entry.target.classList.contains('To_RotatetoLeft')) {
+                entry.target.classList.add('An_RotatetoLeft');
             }
 
         }
